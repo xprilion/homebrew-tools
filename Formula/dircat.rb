d861a192c364a9348c2de9cdfaf150c2f61ef4e4
xprilion <xprilion@gmail.com>
2024-10-21 14:18:20 +0530

    update dircat

--- Formula/dircat.rb
+++ Formula/dircat.rb
@@ -1,12 +1,12 @@
 class Dircat < Formula
     desc "A utility to concatenate files from directories"
     homepage "https://github.com/xprilion/dircat"
-    url "https://github.com/xprilion/dircat/archive/refs/tags/v0.1.0.tar.gz"
-    sha256 "22e7f79c9217e700f0512f71aa867dc14297a0483ebc5d782da6d3fb6bffec5e"
+    url "https://github.com/xprilion/dircat/archive/refs/tags/v0.1.2.tar.gz"
+    sha256 "211fbfb2d7cdbe6e39d645661f02d4b0c6abd640f57c02e6c8f68069081c7288"
     license "MIT"
   
     def install
-      bin.install "dircat"
+      bin.install "bin/dircat"
     end
   
     test do
